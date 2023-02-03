1b6854bc26192bf5e943e23e0b8cda96c15fbb57
Alexcss777 <alexcsslaguna77@gmail.com>
2023-02-02 23:05:01 -0800

    Initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     FormsModule,
     IonicModule,
     HomePageRoutingModule
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import {Chart,registerables} from 'chart.js';
+import { HttpClient } from '@angular/common/http';
+import { WeatherData } from './weather-data.interface';
+Chart.register(...registerables);
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
+  @ViewChild('temperatureChart')
+  temperatureChart!: ElementRef;
+  
+  constructor(private http: HttpClient) {}
+ 
 
-  constructor() {}
-
+  ngOnInit() {
+    this.http.get<WeatherData>(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/241912?apikey=%09PIAcz5bZ3LStSpLy1GsG10hXdqdQcSD3`)
+    .subscribe(data => {
+      const tempData = data.DailyForecasts[0]['Temperature']['Minimum']['Value'];
+      this.createLineChart(tempData);
+    });
+  }
+  
+  createLineChart(tempData: number) {
+    let ctx = this.temperatureChart.nativeElement.getContext('2d');
+    let chartData = [tempData];
+    let chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Current'],
+        datasets: [{
+          label: 'Temperature (Celsius)',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: chartData,
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero:true
+            
+            
+          }
+        }
+      }
+    });
+  }
 }
